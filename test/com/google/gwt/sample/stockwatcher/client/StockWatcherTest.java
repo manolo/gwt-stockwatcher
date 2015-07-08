b5ba7988a4c2e19c9db142a107816d9e04d6e3af
Manolo Carrasco <manolo@apache.org>
2015-07-08 13:55:00 +0200

    Clone from : https://code.google.com/p/google-web-toolkit/downloads/detail?name=Tutorial-GettingStarted-2.1.zip

---
+++ test/com/google/gwt/sample/stockwatcher/client/StockWatcherTest.java
@@ -0,0 +1,24 @@
+package com.google.gwt.sample.stockwatcher.client;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT JUnit tests must extend GWTTestCase.
+ */
+public class StockWatcherTest extends GWTTestCase {
+
+  /**
+   * Must refer to a valid module that sources this class.
+   */
+  public String getModuleName() {
+    return "com.google.gwt.sample.stockwatcher.StockWatcher";
+  }
+
+  /**
+   * Add as many tests as you like.
+   */
+  public void testSimple() {
+    assertTrue(true);
+  }
+
+}
